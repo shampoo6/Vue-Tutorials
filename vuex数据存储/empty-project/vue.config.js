a8922a1754435a5d137b9cbacfd6228b5355592c
shampoo6 <shampoo6@163.com>
2020-08-19 11:45:38 +0800

    完成es6语法和vue环境变量

---
+++ vuex数据存储/empty-project/vue.config.js
@@ -0,0 +1,7 @@
+console.log(process.env.VUE_APP_ENV)
+console.log(process.env.VUE_APP_DEV)
+console.log(process.env.VUE_APP_DEV_LOCAL)
+console.log(process.env.VUE_APP_PRO)
+console.log(process.env.VUE_APP_MYMODE)
+
+module.exports = {}
